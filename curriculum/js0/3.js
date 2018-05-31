7ec129c9fb1cbe66839587d639c2561d10983f24
Henry Linnewehn <nitelife@garagescript.org>
2018-05-30 17:38:37 -0700

    lessonchallenge

--- curriculum/js0/3.js
+++ curriculum/js0/3.js
@@ -1,11 +1,11 @@
-/**
- * Takes in a number, returns true if a number is greater than 5
+/* Takes in a number, returns true if a number is greater than 5
  * @param {number} a
  * @returns {boolean}
  */
 
 const solution = (a) => {
-  return true;
+  if (a === 0 || a < 0 || a === 5) return false;
+  if (a > 5) return true;
 };
 
 module.exports = {
